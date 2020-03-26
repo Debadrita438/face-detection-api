c4dc2714c24aafed6620f0849c34c03e3903e019
Debadrita438 <bristibandyopadhyay@gmail.com>
2020-03-26 11:34:56 +0530

    adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-objective-38697', //heroku database
-    user : 'postgres',
-    password : '',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL, //heroku database
+    ssl: true
   }
 });
 
